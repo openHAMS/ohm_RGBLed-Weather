edeea40d9b8a1742535006678604c40a16244bc6
Székely-Tóth László <notisracer@gmail.com>
2017-06-19 13:23:26 +0200

    comments & formatting

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,37 @@
 
 #include <Task.h>
 #include <Sodaq_BMP085.h>
-#include "taskBMP180.hpp"
+#include "taskBmp180.hpp"
 
 #include "Color.hpp"
 #include "RGBLed.hpp"
 
-#define DEBUG		0
 
 #define WIFI_SSID	"<SSID>"
 #define WIFI_PASS	"<PASS>"
+#define DEBUG        0
 
-#define ALTITUDE	210 // Altitude via GPS - Miskolc, Diósgyőr
+#define ALTITUDE   210 // Altitude via GPS - Miskolc, Diósgyőr
 
-#define LED_RED		12
-#define LED_GREEN	14
-#define LED_BLUE	13
+#define LED_RED     12
+#define LED_GREEN   14
+#define LED_BLUE    13
 
+// [home]/[room]/[passiveFunction]/[device]/[property]
+// [home]/[room]/[ activeFunction]/[ type ]/[location]
+//      home/rcr/sensors/bmp180/pressure
+//      home/rcr/sensors/bmp180/temperature
+//      home/rcr/lights/rgbled/desk
+
+// String room = "home/rcr";
+// String location = "/desk";
+// char* asd;
+// String rgbledAddrset     = room + "/lights/rgbled" + location + "/set";
+// String rgbledAddrstatus  = room + "/lights/rgbled" + location + "/status";
+// String bmp180MQTTatm     = room + "/sensors" + "/bmp180" + "/pressure";
+// String bmp180MQTTtemp    = room + "/sensors" + "/bmp180" + "/temperature";
+
+//mqttConfig qweqwe = mqttConfig("home/rcr", "/desk");
 
 AsyncMqttClient mqttClient;
 RGBLed led = RGBLed(LED_RED, LED_GREEN, LED_BLUE);
@@ -38,22 +53,20 @@ void sendTemp(long a, float t)
 	String(t).toCharArray(temp, sizeof(temp));
 	mqttClient.publish("rcr/rcr/desk/sensors/bmp180/pressure"   , 1, true, atm );
 	mqttClient.publish("rcr/rcr/desk/sensors/bmp180/temperature", 1, true, temp);
-#if DEBUG == 1
-	Serial.print(atm);
-	Serial.print(" ");
-	Serial.println(temp);
-#endif
+    #if DEBUG == 1
+        Serial.println("[DEBUG]->sendData: atm: " + String(atm) + ", temp: " + String(temp));
+    #endif
 }
 
 
 void onMqttConnect()
 {
-	Serial.println("** Connected to the broker **");
-//TODO: [home]/[room]/[device]/[function]/[node]/[property]
-	// [home]/[room]/[device]/[node]/[property]
 	uint16_t packetIdSub = mqttClient.subscribe("rcr/rcr/desk/rgbled/set", 1);
-	Serial.print("Subscribing at QoS 1, packetId: ");
-	Serial.println(packetIdSub);
+    Serial.println("[MQTT] Connected!");
+    #if DEBUG == 1
+        Serial.println("  [DEBUG] Subscribing at QoS 1, packetId: ");
+        Serial.println(packetIdSub);
+    #endif
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
@@ -65,61 +78,62 @@ void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
 
 void onMqttSubscribe(uint16_t packetId, uint8_t qos)
 {
-	Serial.println("** Subscribe acknowledged **");
-	Serial.print("  packetId: ");
-	Serial.println(packetId);
-	Serial.print("  qos: ");
-	Serial.println(qos);
+    Serial.println("[MQTT] Subscribed.");
+    #if DEBUG == 1
+        Serial.println("  [DEBUG] packetId: " + packetId + ", qos: " + qos);
+    #endif
 }
 
 void onMqttUnsubscribe(uint16_t packetId)
 {
-	Serial.println("** Unsubscribe acknowledged **");
-	Serial.print("  packetId: ");
-	Serial.println(packetId);
+    Serial.println("[MQTT] Unsubscribed.");
+    #if DEBUG == 1
+        Serial.println("  [DEBUG] packetId: " + packetId);
+    #endif
 }
 
 void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
 {
-#if DEBUG == 1
-	Serial.println("** Publish received **");
-	Serial.print("  payload: ");
-	Serial.println(payload);
-	Serial.print("  color: ");
-	Color color = ColorParse(payload);
-	Serial.print(color.R);
-	Serial.print(":");
-	Serial.print(color.G);
-	Serial.print(":");
-	Serial.print(color.B);
-	Serial.print(":");
-	Serial.println(color.A);
-	Serial.print("  topic: ");
-	Serial.println(topic);
-	Serial.print("  qos: ");
-	Serial.println(properties.qos);
-	Serial.print("  dup: ");
-	Serial.println(properties.dup);
-	Serial.print("  retain: ");
-	Serial.println(properties.retain);
-	Serial.print("  len: ");
-	Serial.println(len);
-	Serial.print("  index: ");
-	Serial.println(index);
-	Serial.print("  total: ");
-	Serial.println(total);
-#endif
-	led.setColor(ColorParse(payload));
-	mqttClient.publish("rcr/rcr/desk/rgbled/status", 1, true, payload);
+    /*
+    #if DEBUG == 1
+        Serial.println("[MQTT] Message received");
+        Serial.print("  payload: ");
+        Serial.println(payload);
+        Serial.print("  color: ");
+        Color color = ColorParse(payload);
+        Serial.print(color.R);
+        Serial.print(":");
+        Serial.print(color.G);
+        Serial.print(":");
+        Serial.print(color.B);
+        Serial.print(":");
+        Serial.println(color.A);
+        Serial.print("  topic: ");
+        Serial.println(topic);
+        Serial.print("  qos: ");
+        Serial.println(properties.qos);
+        Serial.print("  dup: ");
+        Serial.println(properties.dup);
+        Serial.print("  retain: ");
+        Serial.println(properties.retain);
+        Serial.print("  len: ");
+        Serial.println(len);
+        Serial.print("  index: ");
+        Serial.println(index);
+        Serial.print("  total: ");
+        Serial.println(total);
+    #endif
+    if (topic == ledAddr) {
+        led.setColor(ColorParse(payload));
+    }
+    */
 }
 
 void onMqttPublish(uint16_t packetId)
 {
-#if DEBUG == 1
-		Serial.println("** Publish acknowledged **");
-		Serial.print("  packetId: ");
-		Serial.println(packetId);
-#endif
+    #if DEBUG == 1
+        Serial.println("[MQTT] Message Sent, packetId: " + packetId);
+    #endif
 }
 
 void setup()
