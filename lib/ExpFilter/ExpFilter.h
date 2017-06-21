d8cda06327bdcee5566a2e7a7e20f141b24beb0b
Székely-Tóth László <notisracer@gmail.com>
2017-06-21 16:21:03 +0200

    implemented exponential smoothing for bmp180 sensor

---
+++ lib/ExpFilter/ExpFilter.h
@@ -0,0 +1,6 @@
+#ifndef EXPFILTER_HPP
+#define EXPFILTER_HPP
+
+#include "ExpFilter.hpp"
+
+#endif /* EXPFILTER_HPP */
--- src/taskBMP180.hpp
+++ src/taskBMP180.hpp
@@ -1,53 +1,30 @@
+#ifndef TASKBMP180_HPP
+#define TASKBMP180_HPP
+
 #include <Arduino.h>
 #include <Task.h>
 #include <Wire.h>
 #include <Sodaq_BMP085.h>
+#include <ExpFilter.h>
 
 class TaskReadWeather : public Task
 {
 public:
     typedef void(*action)(long a, float t);
-    TaskReadWeather(action function, unsigned int alt, uint32_t timeInterval):
-        Task(timeInterval),
-        _ALTITUDE(alt),
-        _callback(function)
-    {
-        _sensor.begin();
-    }
+    TaskReadWeather(action function, uint16_t alt, uint32_t timeInterval);
 
 private:
-    const action _callback;
-    const unsigned int _ALTITUDE;
-    Sodaq_BMP085 _sensor;
-
+    const action callback;
+    const uint16_t ALTITUDE;
     int counter;
+    bool filterInited = false;
+    Sodaq_BMP085 sensor;
+    ExpFilter<long> atmFilter;
+    ExpFilter<float> tempFilter;
 
-    virtual bool OnStart()
-    {
-        counter = 0;
-        #if DEBUG == 1
-            Serial.println("[SENSOR] task started...");
-        #endif
-        return true;
-    }
-
-    virtual void OnStop()
-    {
-        #if DEBUG == 1
-            Serial.println("[SENSOR] task stopped.");
-        #endif
-    }
-
-    virtual void OnUpdate(uint32_t deltaTime)
-    {
-        #if DEBUG == 1
-            Serial.print("[SENSOR] update ");
-            Serial.print(counter);
-            Serial.println();
-        #endif
-        counter++;
-        float t = _sensor.readTemperature();
-        long a = _sensor.readPressure(_ALTITUDE);
-        _callback(a, t);
-    }
+    virtual bool OnStart();
+    virtual void OnStop();
+    virtual void OnUpdate(uint32_t deltaTime);
 };
+
+#endif /* TASKBMP180_HPP */
