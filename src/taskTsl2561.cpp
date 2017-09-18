c84d507b98ff0eeb96c3220c214e5b63d831e533
Székely-Tóth László <notisracer@gmail.com>
2017-09-18 21:24:11 +0200

    light sensor tsl2561 added

---
+++ src/taskTsl2561.cpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include "taskTsl2561.hpp"
+#include <ExpFilter.h>
+
+TaskReadLight::TaskReadLight(char* addr, action function, uint32_t timeInterval):
+    Task(timeInterval),
+    address(addr),
+    lightFilter(25, 10),
+    callback(function)
+{
+    sensor.begin();
+}
+
+bool TaskReadLight::OnStart()
+{
+    if(!sensor.begin())
+    {
+        return false;
+    }
+    // setup config
+    sensor.enableAutoRange(true);
+    sensor.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);
+    // read initial value
+    sensors_event_t event;
+    sensor.getEvent(&event);
+    lightFilter.SetValue(event.light);
+    // start successful
+    return true;
+}
+
+void TaskReadLight::OnStop()
+{
+
+}
+
+void TaskReadLight::OnUpdate(uint32_t deltaTime)
+{
+    // read sensor
+    sensors_event_t event;
+    sensor.getEvent(&event);
+    // if reading ok, else it is oversaturated
+    if (event.light)
+    {
+        lightFilter.Filter(event.light);
+    }
+    // convert to string
+    char light[8];
+    String(lightFilter.Current()).toCharArray(light, sizeof(light));
+    // send data
+    callback(address, light);
+}
